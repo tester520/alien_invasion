f93b4f9bcdef34078d22508486f58df6e6f929ae
sheng <sheng@qq.com>
2022-04-06 18:57:26 +0800

    第三次提交

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 from settings import Settings
-
+from ship import Ship
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -14,6 +14,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
         # 设置背景色。
         self.bg_color = (230, 230, 230)
@@ -28,6 +30,7 @@ class AlienInvasion:
 
             # 每次循环时都重绘屏幕。
             self.screen.fill(self.bg_color)
+            self.ship.blitme()
 
             # 让最近绘制的屏幕可见。
             pygame.display.flip()
